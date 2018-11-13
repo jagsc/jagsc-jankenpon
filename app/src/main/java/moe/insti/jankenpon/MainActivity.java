c26e98d78d31bbbd2c951eb0506ba940489a069c
Ayumu Yuki <ayumu.helloworld@gmail.com>
2018-11-13 13:33:18 +0900

    画像表示機能を追加

--- app/src/main/java/moe/insti/jankenpon/MainActivity.java
+++ app/src/main/java/moe/insti/jankenpon/MainActivity.java
@@ -1,29 +1,61 @@
 package moe.insti.jankenpon;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private JanKenPon JanKenPon;
     private TextView textUser, textCpu, textResult;
+    private Bitmap rockBitmap, scissorsBitmap, paperBitmap;
+    private ImageView resultImageView;
 
-    //ボタンが押されてるかどうかを確認するリスナーの追加
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        // Viewとレイアウトの紐付け
+        textUser = findViewById(R.id.textUser);
+        textCpu = findViewById(R.id.textCpu);
+        textResult = findViewById(R.id.textResult);
+        resultImageView = findViewById(R.id.resultImage);
+
         Button rock = findViewById(R.id.buttonRock);
         Button scissors = findViewById(R.id.buttonScissors);
         Button paper = findViewById(R.id.buttonPaper);
 
+        // ボタンが押されてるかどうかを確認するリスナーの追加
         rock.setOnClickListener(this);
         scissors.setOnClickListener(this);
         paper.setOnClickListener(this);
+
+        // 画像をassetsから読み込み
+        AssetManager assets = getResources().getAssets();
+
+        try {
+            InputStream inputStream = assets.open("rock.png");
+            rockBitmap = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+            inputStream = assets.open("scissors.png");
+            scissorsBitmap = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+            inputStream = assets.open("paper.png");
+            paperBitmap = BitmapFactory.decodeStream(inputStream);
+            inputStream.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     //結果を表示するメソッド
@@ -31,14 +63,23 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         String result;
         JanKenPon = new JanKenPon();
 
-        textUser = findViewById(R.id.textUser);
-        textCpu = findViewById(R.id.textCpu);
-        textResult = findViewById(R.id.textResult);
-
         result = JanKenPon.JanKenPon(userAction);
         textUser.setText("あなたは" + userAction + "を選択しました。");
         textCpu.setText("CPUが" + JanKenPon.randomAction + "を出しました。");
         textResult.setText("結果："+ result + "！");
+
+        // 結果の画像を設定する
+        switch (JanKenPon.randomAction){
+            case "グー":
+                resultImageView.setImageBitmap(rockBitmap);
+                break;
+            case "チョキ":
+                resultImageView.setImageBitmap(scissorsBitmap);
+                break;
+            case "パー":
+                resultImageView.setImageBitmap(paperBitmap);
+                break;
+        }
     }
 
     //ボタンが押されると結果を表示するメソッド
