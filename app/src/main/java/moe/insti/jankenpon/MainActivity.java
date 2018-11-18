ad954b0a77137301a053c76034513f3cea2287fb
Ayumu Yuki <s1f101704152@iniad.org>
2018-11-19 01:33:21 +0900

    画像の読み込み元をAssetsからres/drawableに修正しプログラムをわかりやすくした
    画像のサイズが大き過ぎてdrawableに置くとエラーが出たのでサンプル画像サイズも修正

--- app/src/main/java/moe/insti/jankenpon/MainActivity.java
+++ app/src/main/java/moe/insti/jankenpon/MainActivity.java
@@ -1,8 +1,5 @@
 package moe.insti.jankenpon;
 
-import android.content.res.AssetManager;
-import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -10,13 +7,11 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 
-import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
     private JanKenPon JanKenPon;
     private TextView textUser, textCpu, textResult;
-    private Bitmap rockBitmap, scissorsBitmap, paperBitmap;
     private ImageView resultImageView;
 
 
@@ -39,12 +34,6 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         rock.setOnClickListener(this);
         scissors.setOnClickListener(this);
         paper.setOnClickListener(this);
-
-        // 画像をassetsから読み込み
-        AssetManager assets = getResources().getAssets();
-        rockBitmap = getBitmap(assets,"rock.png");
-        scissorsBitmap = getBitmap(assets,"scissors.png");
-        paperBitmap = getBitmap(assets,"paper.png");
     }
 
     //結果を表示するメソッド
@@ -60,13 +49,13 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
         // 結果の画像を設定する
         switch (JanKenPon.randomAction){
             case "グー":
-                resultImageView.setImageBitmap(rockBitmap);
+                resultImageView.setImageResource(R.drawable.rock);
                 break;
             case "チョキ":
-                resultImageView.setImageBitmap(scissorsBitmap);
+                resultImageView.setImageResource(R.drawable.scissors);
                 break;
             case "パー":
-                resultImageView.setImageBitmap(paperBitmap);
+                resultImageView.setImageResource(R.drawable.paper);
                 break;
         }
     }
@@ -87,14 +76,5 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 showResult("パー");
                 break;
         }
-    }
-
-    public Bitmap getBitmap(AssetManager assets,String filename){
-        try (InputStream inputStream = assets.open(filename)){
-            return BitmapFactory.decodeStream(inputStream);
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
-        return null;
     }
 }
