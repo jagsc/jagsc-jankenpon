b9def93d1e5ea1c81e84db99191de0783edb4c01
Ayumu Yuki <s1f101704152@iniad.org>
2018-11-29 03:50:39 +0900

    3章最初の状態のコード

--- app/src/main/java/com/example/jankenpon/MainActivity.java
+++ app/src/main/java/com/example/jankenpon/MainActivity.java
@@ -3,16 +3,12 @@ package com.example.jankenpon;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextView textUser, textCpu, textResult;
-    private ImageView resultImageView;
-
+    private TextView textUser;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,57 +17,20 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         // Viewとレイアウトの紐付け
         textUser = findViewById(R.id.textUser);
-        textCpu = findViewById(R.id.textCpu);
-        textResult = findViewById(R.id.textResult);
-        resultImageView = findViewById(R.id.resultImage);
 
-        Button rock = findViewById(R.id.buttonRock);
-        Button scissors = findViewById(R.id.buttonScissors);
-        Button paper = findViewById(R.id.buttonPaper);
-
-        // ボタンが押されてるかどうかを確認するリスナーの追加
-        rock.setOnClickListener(this);
-        scissors.setOnClickListener(this);
-        paper.setOnClickListener(this);
     }
 
-    /**
-     * じゃんけんを実行し結果を表示する
-     */
-    public void showResult(String userAction) {
-        JanKenPon janKenPon = new JanKenPon();
-
-        String result = janKenPon.doJanKenPon(userAction);
-        textUser.setText("あなたは" + userAction + "を選択しました。");
-        textCpu.setText("CPUが" + janKenPon.cpuAction + "を出しました。");
-        textResult.setText("結果："+ result + "！");
-
-        // 結果の画像を設定する
-        switch (janKenPon.cpuAction){
-            case "グー":
-                resultImageView.setImageResource(R.drawable.rock);
-                break;
-            case "チョキ":
-                resultImageView.setImageResource(R.drawable.scissors);
-                break;
-            case "パー":
-                resultImageView.setImageResource(R.drawable.paper);
-                break;
-        }
-    }
-
-    //ボタンが押されると結果を表示するメソッド
     @Override
     public void onClick(View view) {
         switch (view.getId()) {
             case R.id.buttonRock:
-                showResult("グー");
+                textUser.setText("あなたはグーを選択しました");
                 break;
             case R.id.buttonScissors:
-                showResult("チョキ");
+                textUser.setText("あなたはチョキを選択しました");
                 break;
             case R.id.buttonPaper:
-                showResult("パー");
+                textUser.setText("あなたはパーを選択しました");
                 break;
         }
     }
